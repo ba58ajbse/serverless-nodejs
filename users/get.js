8f53b5ae20b43c841a734ac069a081cbc6166736
shibuya <shibuya@pyxos-jk.co.jp>
2021-07-01 14:50:51 +0900

    add get item function

---
+++ users/get.js
@@ -0,0 +1,42 @@
+const AWS = require('aws-sdk');
+const dynamodb = process.env.IS_OFFLINE
+  ? new AWS.DynamoDB.DocumentClient({
+      region: 'localhost',
+      endpoint: 'http://localhost:8000',
+    })
+  : new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+  let statusCode = 200;
+  let body;
+  const headers = { 'Content-Type': 'application/json' };
+  const id = Number(event.pathParameters.id);
+
+  if (!id) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify('Forbidden'),
+      headers,
+    };
+  }
+
+  const params = {
+    TableName: 'users',
+    Key: { id },
+  };
+
+  try {
+    const data = await dynamodb.get(params).promise();
+    statusCode = 200;
+    body = JSON.stringify(data.Item);
+  } catch (error) {
+    statusCode = error.statusCode;
+    body = JSON.stringify({ statusCode, error: error.message });
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
